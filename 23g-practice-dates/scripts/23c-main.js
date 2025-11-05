6a094bcf119a4373c0387bc81a2e85aebafdb28b
53058vincent-ai <53058vincent@mustangps.org>
2025-11-05 13:31:23 -0600

    Add files via upload

---
+++ 23g-practice-dates/scripts/23c-main.js
@@ -0,0 +1,7 @@
+﻿const salesTaxRate = .075;
+incMsg = "This site is under construction and will be fully functional soon.";
+
+function Product(item,size,price){
+this.item = item;
+this.size = size;
+this.price = price;}// insert product class here
